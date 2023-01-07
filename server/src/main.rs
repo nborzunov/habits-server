644fcc393c589a37960a3ee04bf0810acf40df0f
Nikolay Borzunov <forest3694@ya.ru>
2023-01-07 17:58:57 +0400

    Backend delete habit endpoint fix

--- server/src/main.rs
+++ server/src/main.rs
@@ -28,7 +28,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(
                 Cors::default()
                     .allowed_origin("http://localhost:3000")
-                    .allowed_methods(vec!["GET", "POST"])
+                    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
                     .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
                     .allowed_header(header::CONTENT_TYPE)
                     .supports_credentials()
