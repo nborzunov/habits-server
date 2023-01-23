c44ca47ff93a602beff030e61ce9d38445c08cd5
Nikolay Borzunov <forest3694@ya.ru>
2023-01-23 13:22:59 +0400

    Fixed env variable

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,14 +1,22 @@
-use actix_web::{web, Scope};
+use actix_web::{HttpResponse, Scope, web};
 
 pub mod auth;
 pub mod habits;
 pub mod targets;
 pub mod users;
 
+async fn init() -> HttpResponse {
+    HttpResponse::Ok().body("Server works!")
+}
+
+
+
 pub fn routes() -> Scope {
     web::scope("")
+        .service(habits::routes())
         .service(habits::routes())
         .service(targets::routes())
         .service(auth::routes())
         .service(users::routes())
+        .route("/", web::get().to(init))
 }
